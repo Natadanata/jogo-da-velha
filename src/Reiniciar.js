5c9dbca343932c57fcbef87d0b194707a6b563e9
Natanael Francisco de Lima <abecedariovalente@gmail.com>
2026-06-08 23:52:28 -0300

    Update Reiniciar.js

--- src/Reiniciar.js
+++ src/Reiniciar.js
@@ -1,10 +1,23 @@
-export function BotaoReiniciar({setQuadrados, setEstado, setStatus}) {
- 
- function reiniciarJogo() {
+export function BotaoReiniciar({
+  setQuadrados,
+  setEstado,
+  setStatus,
+  setHistorico,
+  setVencedores
+}) {
+  function reiniciarJogo() {
     setQuadrados(Array(9).fill(null));
     setEstado(false);
     setStatus(null);
+    setHistorico([]);
+    setVencedores([]);
   }
 
-  return <><div><button onClick={reiniciarJogo}>Reiniciar</button></div></>
+  return (
+    <div>
+      <button onClick={reiniciarJogo}>
+        Reiniciar
+      </button>
+    </div>
+  );
 }
