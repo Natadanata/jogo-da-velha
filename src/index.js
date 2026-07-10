cd0cb5f2690cc1d9ad2e1a9761e4c80b45087880
Anna.projeto.gitrub@gmail.com <anna.projeto.gitrub@gmail.com>
2026-07-10 13:43:20 -0300

    Comentarios gerais

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles.css";
 
 import App from "./app";
 
+/* Cria a raiz do React apontando para o elemento HTML 'root' e renderiza o componente principal <App /> dentro do StrictMode para validações em desenvolvimento */
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
